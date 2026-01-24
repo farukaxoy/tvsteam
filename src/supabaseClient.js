d092fdd782c3135f8cb2f481fd1d3f64be4540cf
farukaxoy <farukaxoy@gmail.com>
2026-01-25 01:34:12 +0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log("SB URL", supabaseUrl);
+console.log("SB ANON", (supabaseAnonKey || "").slice(0, 12));
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
